b844ba6fb111ddcc173a78bcabac8726c9c2e696
Emi-10 <63738625+Emi-10@users.noreply.github.com>
2020-04-15 16:29:26 -0300

    Add files via upload

---
+++ Assets/Script/MovAutomatico.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovAutomatico : MonoBehaviour
+{
+    bool izquierda = false;
+    public int velocidad;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //ponele este script al cubo
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (izquierda == false)
+        {
+
+            transform.Translate(Vector3.right * velocidad * Time.deltaTime);
+        }
+        if(transform.position.x > 11.82f) {
+
+            izquierda = true;
+        }
+        if (izquierda == true) { 
+
+            transform.Translate(Vector3.left * velocidad * Time.deltaTime);
+        }
+        if (transform.position.x < 11.82f) { 
+
+            izquierda = false;
+        }
+    }
+}
