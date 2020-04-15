770113cf17dd9c4a78a5a741835445501068d690
Emi-10 <63738625+Emi-10@users.noreply.github.com>
2020-04-15 16:30:38 -0300

    Update and rename Assets/Script/Hola.cs to  Activos/ Script/Hola.cs

--- Activos/ Script/Hola.cs
+++ Activos/ Script/Hola.cs
@@ -7,7 +7,7 @@ public class Hola : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log("HolaMundo");
     }
 
     // Update is called once per frame
